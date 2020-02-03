fa66ff24d705aa338926add132d753d0c6abfd45
KimDaeUng <41335296+KimDaeUng@users.noreply.github.com>
2020-02-03 15:00:53 +0900

    Update MAML.py

---
+++ models/optimization_based/MAML.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+import os
+import shutil
+import numpy as np
+import random
+tf.enable_eager_execution()
+
+class MAML():
+    def __init__(self, image_size, N, meta_lr = 1e-4, train_lr=1e-3):
+        self.image_size = image_size
+        self.N = N
+        self.meta_lr = meta_lr
+        self.train_lr = train_lr
+       
