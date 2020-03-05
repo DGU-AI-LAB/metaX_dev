0c6cc3d30866efe1b47272d3b22b945f08857802
sanghyuckNa <ksj01357@naver.com>
2020-03-05 12:19:48 +0900

    data loader
    data loader tensorflow 2.0 version

--- datasets/data_generator_2.0.py
+++ datasets/data_generator_2.0.py
@@ -113,35 +113,36 @@ class DataGenerator():
             folders = self.metatest_character_folders
             total_batch = self.num_test
 
-        all_filenames, all_labels = [], []
+        self.all_filenames, self.all_labels = [], []
         for _ in tqdm.trange(0, total_batch):
             sampled_character_folders = random.sample(folders, self.N)
             random.shuffle(sampled_character_folders)
             labels_and_images = self.get_images(sampled_character_folders, range(self.N), K=self.K,  shuffle=False)
             # # make sure the above isn't randomized order
-            labels = [li[0] for li in labels_and_images]
-            filenames = [li[1] for li in labels_and_images]
-            all_filenames.extend(filenames)
-            all_labels.extend(labels)
+            self.labels = [li[0] for li in labels_and_images]
+            self.filenames = [li[1] for li in labels_and_images]
+            self.all_filenames.extend(self.filenames)
+            self.all_labels.extend(self.labels)
 
-        all_filenames = tf.convert_to_tensor(all_filenames)
-        dataset = tf.data.Dataset.from_tensor_slices(all_filenames)
-        dataset = dataset.map(
+        self.all_filenames = tf.convert_to_tensor(self.all_filenames)
+        self.dataset = tf.data.Dataset.from_tensor_slices(self.all_filenames)
+        self.dataset = self.dataset.map(
             lambda data_list: tf.py_function(_read_py_function, [data_list], [tf.int32]))
-        dataset = dataset.map(_resize_function)
+        self.dataset = self.dataset.map(_resize_function)
 
-        task_image_size = (self.N * self.K) * 2
-        batch_image_size = self.batch_size * task_image_size
+        self.task_image_size = (self.N * self.K) * 2
+        self.batch_image_size = self.batch_size * self.task_image_size
         # print('Batching images')
-        dataset = dataset.batch(batch_image_size)
+        self.dataset = self.dataset.batch(self.batch_image_size)
 
+    def next(self):
         support_batch_data, support_batch_labels, query_batch_data, query_batch_labels = [], [], [], []
         # print('Manipulating image data to be right shape')
 
         # index choices support & query
         query_task_idx = []
-        idx_list = np.array(range(task_image_size))
-        reshape_idx_list = idx_list.reshape(task_image_size // (self.K*2), self.K*2)
+        idx_list = np.array(range(self.task_image_size))
+        reshape_idx_list = idx_list.reshape(self.task_image_size // (self.K*2), self.K*2)
 
         for class_idx in range(reshape_idx_list.shape[0]):
             query_task_idx.extend(reshape_idx_list[class_idx][:self.K])
@@ -149,29 +150,30 @@ class DataGenerator():
         support_task_idx = [x for x in idx_list if x not in temp_list]
 
         # task 별로 뽑아내기
-        it = iter(dataset)
+        it = iter(self.dataset)
+
         for num_task in range(self.batch_size):
             images = it.next()
             support_task_images, support_task_labels = [], []
             query_task_images, query_task_labels = [], []
             for support_idx in support_task_idx:
                 support_task_images.append(images[support_idx])
-                support_task_labels.append(labels[support_idx])
+                support_task_labels.append(self.labels[support_idx])
             for query_idx in query_task_idx:
                 query_task_images.append(images[query_idx])
-                query_task_labels.append(labels[query_idx])
-
+                query_task_labels.append(self.labels[query_idx])
             support_batch_data.append(support_task_images)
-            # print(tf.one_hot(support_task_labels, self.N))
             support_batch_labels.append(tf.one_hot(support_task_labels, self.N))
             query_batch_data.append(query_task_images)
             query_batch_labels.append(tf.one_hot(query_task_labels, self.N))
 
-        support_batch_data = tf.stack(support_batch_data)
-        support_batch_labels = tf.stack(support_batch_labels)
-        query_batch_data = tf.stack(query_batch_data)
-        query_batch_labels = tf.stack(query_batch_labels)
-        return support_batch_data, support_batch_labels, query_batch_data, query_batch_labels
+        # support_batch_data = tf.stack(support_batch_data)
+        # support_batch_labels = tf.stack(support_batch_labels)
+        # query_batch_data = tf.stack(query_batch_data)
+        # query_batch_labels = tf.stack(query_batch_labels)
+
+        # return support_batch_data, support_batch_labels, query_batch_data, query_batch_labels
+        return zip(support_batch_data, support_batch_labels, query_batch_data, query_batch_labels)
 
 
 
@@ -193,11 +195,13 @@ if __name__ == '__main__':
     batch_size = 32
     image_resize = (28, 28)
     data_generator = DataGenerator(image_resize, N=n, K=k, batch_size=batch_size)
-    support_batch_data, support_batch_labels, query_batch_data, query_batch_labels = data_generator.make_data_tensor()
-    print(support_batch_data.shape)
-    print(support_batch_labels.shape)
-    print(query_batch_data.shape)
-    print(query_batch_labels.shape)
+    data_generator.make_data_tensor()
+    datas = data_generator.next()
+    support_batch_data, support_batch_labels, query_batch_data, query_batch_labels = zip(*datas)
+    print(tf.stack(support_batch_data).shape)
+    print(tf.stack(support_batch_labels).shape)
+    print(tf.stack(query_batch_data).shape)
+    print(tf.stack(query_batch_labels).shape)
     import matplotlib.pyplot as plt
 
     if not os.path.isdir('metaX/support/'):
@@ -205,10 +209,10 @@ if __name__ == '__main__':
     if not os.path.isdir('metaX/query/'):
         os.mkdir('metaX/query/')
 
-    for k in range(batch_size):
-        for i in range(support_batch_data.shape[1]):
-            plt.imshow(support_batch_data[k][i].numpy().reshape(28, 28), cmap="gray")
-            plt.savefig("metaX/support/{}_{}.jpg".format(k, i))
-        for j in range(query_batch_data.shape[1]):
-            plt.imshow(query_batch_data[k][j].numpy().reshape(28, 28), cmap="gray")
-            plt.savefig("metaX/query/{}_{}.jpg".format(k, j))
+    # for k in range(batch_size):
+    #     for i in range(support_batch_data.shape[1]):
+    #         plt.imshow(support_batch_data[k][i].numpy().reshape(28, 28), cmap="gray")
+    #         plt.savefig("metaX/support/{}_{}.jpg".format(k, i))
+    #     for j in range(query_batch_data.shape[1]):
+    #         plt.imshow(query_batch_data[k][j].numpy().reshape(28, 28), cmap="gray")
+    #         plt.savefig("metaX/query/{}_{}.jpg".format(k, j))
