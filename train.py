26e2396a65a02ea44d0ea8a96fde64a5a697439c
KimDaeUng <41335296+KimDaeUng@users.noreply.github.com>
2020-07-03 13:39:54 +0900

    2020.06.29. Distributed version

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
-from model.optimization_based.MAML import OmniglotModel, MiniImagenetModel, ModelAgnosticMetaLearningModel
-from model.model_based.MANN import MANNModel, MetaLearningMemoryAugmentedNeuralNet
-from dataset.data_generator_MAML import OmniglotDatabase, MiniImagenetDatabase
+from model.optimization_based.MAML import OmniglotModel, MiniImagenetModel, ModelAgnosticMetaLearning
+from dataset.data_generator import OmniglotDatabase, MiniImagenetDatabase
 import argparse
 
 import logging, os
@@ -8,18 +7,20 @@ import logging, os
 logging.disable(logging.WARNING)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
+
 if __name__ == '__main__':
+    # 학습에 필요한 파라미터를 입력으로 받습니다.
     parser = argparse.ArgumentParser()
-    parser.add_argument('--database', type=str, default='mini_imagenet')
-    parser.add_argument('--network_cls', type=str, default='mini_imagenet')
-    # parser.add_argument('--database', type=str, default='omniglot')
-    # parser.add_argument('--network_cls', type=str, default='omniglot')
+    # parser.add_argument('--benchmark_dataset', type=str, default='mini_imagenet')
+    # parser.add_argument('--network_cls', type=str, default='mini_imagenet')
+    parser.add_argument('--benchmark_dataset', type=str, default='omniglot')
+    parser.add_argument('--network_cls', type=str, default='omniglot')
     parser.add_argument('--n', type=int, default=5)
     parser.add_argument('--epochs', type=int, default=5)
     parser.add_argument('--iterations', type=int, default=5)
     parser.add_argument('--k', type=int, default=1)
     # parser.add_argument('--meta_batch_size', type=int, default=32)
-    parser.add_argument('--meta_batch_size', type=int, default=4)
+    parser.add_argument('--meta_batch_size', type=int, default=2)
     parser.add_argument('--num_steps_ml', type=int, default=10)
     parser.add_argument('--lr_inner_ml', type=float, default=0.4)
     parser.add_argument('--num_steps_validation', type=int, default=10)
@@ -30,62 +31,48 @@ if __name__ == '__main__':
     parser.add_argument('--log_train_images_after_iteration', type=int, default=1)
 
 
-    # setting1 하나의 디렉토리에 모든 파일이 있을 때
-    # setting2 train, val, test 나뉘었을 때
-
-    # Data I/O database, raw_data_address
-
-    # Pre-processing # n, k, meta_batch_size
-
-    # Analytics # database(object), network_cls(object), n, k,
-        # meta_batch_size, num_steps_ml, lr_inner_ml, num_steps_validation,
-        # save_after_epochs, meta_learning_rate, report_validation_frequency,
-        # log_train_image_after_iteration
-
-
-
     args = parser.parse_args()
-    
-    if args.database == "omniglot":
+
+    # 데이터셋 객체를 생성합니다.
+    # 타입 : tf.data.Dataset
+    if args.benchmark_dataset == "omniglot":
         database = OmniglotDatabase(
             raw_data_address="dataset\omniglot",
             random_seed=47,
             num_train_classes=1200,
             num_val_classes=100)
-    elif args.database == "mini_imagenet":
+    elif args.benchmark_dataset == "mini_imagenet":
         database=MiniImagenetDatabase(
             raw_data_address="\dataset\mini_imagenet",
             random_seed=-1)
 
+    # 모델 객체를 생성합니다.
     if args.network_cls == "omniglot":
         network_cls=OmniglotModel
     elif args.network_cls == "mini_imagenet":
         network_cls=MiniImagenetModel
 
-    # train_dict, val_dict, test_dict = database.get_class()
-    # print(train_dict.keys())
-    # keys_list = list(train_dict.keys())
-    # print(database.preview_image(train_dict[keys_list[0]][0]))
-    # print(database.preview_image(train_dict['n01532829'][0]))
+    # 학습을 위한 클래스를 생성합니다.
+    maml = ModelAgnosticMetaLearning(args, database, network_cls)
+    # args : 파라미터      type : parser.parse_args
+    # database : 데이터셋  type : database
+    # network_cls : 모델   type : MetaLearning
+    
+    # 학습을 위한 클래스를 사용하여 입력받은 파라미터를 통해 meta_train을 수행합니다.
+    maml.meta_train(epochs = args.epochs)
+    # epochs : 반복 횟수 type : int
 
+    # meta_test 시 입력받은 파라미터를 통해 fine turning할 횟수 만큼 수행합니다.
+    maml.meta_test(iterations = args.iterations)
+    # iterations : inner loop의 gradient update 횟수 type : int
 
-    maml = ModelAgnosticMetaLearningModel(args, database, network_cls)
-    # maml.train(epochs = args.epochs)
-    # maml.evaluate(iterations = args.iterations)
-    # maml.load_model(args.epochs)
-    # print(maml.predict('/dataset/data/mini_imagenet/test'))
+    # 입력받은 파라미터를 통해 해당 epochs의 저장된 모델을 불러옵니다.
+    maml.load_model(epochs = args.epochs)
+    # epochs : 몇번 학습한 모델을 불러올 지  type : int
+    # None일 시 최종 학습한 모델을 불러옵니다.
 
-
-
-
-
-    # maml.evaluate(iterations = args.iterations)
-    # print(database.test_folders)
-
-    # print(maml.predict())
-
-    # network_cls=MANNModel
-    # mann = MetaLearningMemoryAugmentedNeuralNet(args, database, network_cls)
-    # mann.train(epochs=args.epochs)
-    # mann.evaluate(iterations = args.iterations)
-    # print(mann.predict())
+    # 예측한 결과를 보여줍니다.
+    print(maml.predict_with_support(meta_test_path='/dataset/data/mini_imagenet/test'))
+    # meta_test_path 예측할 데이터의 경로               type : string
+    # epochs_to_load_from 몇번 학습한 모델을 볼러올지   type : int  None일 시 최종 학습한 모델을 불러옵니다.
+    # iterations fine turning 횟수                    type : int
